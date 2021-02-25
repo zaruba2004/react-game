86089da39a86bdbf71f19f8ee89d62e7684bb73c
zaruba2004 <zaruba2004gmail.com>
2021-02-25 13:33:58 +0300

    add modal in mainGame

--- src/mainGame/mainGame.js
+++ src/mainGame/mainGame.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './mainGame.css';
-import Modal from "./modal";
+// import Modal from "./modal";
+import './modal.css';
 
 export default function MainGame() {
     const [cards, setCards] = React.useState([
@@ -20,19 +21,18 @@ export default function MainGame() {
         {id: 14, imageURL: './images/KH.png', title: "KH"},
         {id: 15, imageURL: './images/KS.png', title: "KS"}
       ])
+
+      const [counter, setCounter] = useState(0);
       
       function clickOneButton(arr){  
+        setCounter(counter + 1);  
         let a = [];
         let b = [];
         let c = [];
-        for (let i = 0; i < arr.length; i=i+3) {
-            a.push(arr[i])            
-        }
-        for (let i = 1; i < arr.length; i=i+3) {            
-            b.push(arr[i])           
-        }
-        for (let i = 2; i < arr.length; i=i+3) {
-            c.push(arr[i])            
+        for (let i = 0; i < arr.length; i+=3) {
+            a.push(arr[i])
+            b.push(arr[i+1])
+            c.push(arr[i+2])
         }
         setCards(arr = []);
         arr.push(...b);
@@ -42,17 +42,14 @@ export default function MainGame() {
       }
 
       function clickTwoButton(arr){  
+        setCounter(counter + 1);
         let a = [];
         let b = [];
         let c = [];
-        for (let i = 0; i < arr.length; i=i+3) {
-            a.push(arr[i])            
-        }
-        for (let i = 1; i < arr.length; i=i+3) {            
-            b.push(arr[i])           
-        }
-        for (let i = 2; i < arr.length; i=i+3) {
-            c.push(arr[i])            
+        for (let i = 0; i < arr.length; i+=3) {
+            a.push(arr[i])
+            b.push(arr[i+1])
+            c.push(arr[i+2])
         }
         setCards(arr = []);
         arr.push(...a);
@@ -61,24 +58,32 @@ export default function MainGame() {
         return arr;
       }
 
-      function clickTreeButton(arr){  
+      function clickTreeButton(arr){ 
+        setCounter(counter + 1); 
         let a = [];
         let b = [];
         let c = [];
-        for (let i = 0; i < arr.length; i=i+3) {
-            a.push(arr[i])            
-        }
-        for (let i = 1; i < arr.length; i=i+3) {            
-            b.push(arr[i])           
-        }
-        for (let i = 2; i < arr.length; i=i+3) {
-            c.push(arr[i])            
-        }
+        for (let i = 0; i < arr.length; i+=3) {
+            a.push(arr[i])
+            b.push(arr[i+1])
+            c.push(arr[i+2])
+        }        
         setCards(arr = []);
         arr.push(...a);
         arr.push(...c);
         arr.push(...b);
         return arr;
+      }    
+    
+
+      const [state, setState] = useState (    /* temp modal */
+        {isOpenModal: false, isOpenButton: false, isOpenButtons: true}
+    )
+
+
+      if(counter==3){
+        setCounter(0);
+        setState({isOpenModal: false, isOpenButton: true, isOpenButtons: false, });        
       }
 
     return (
@@ -91,12 +96,22 @@ export default function MainGame() {
                 </ul> 
             </div>
             <div className="buttonsField">
-                <div className="startButtonsField">
-                <button onClick={()=>setCards(clickOneButton(cards))}>Карта в этой коллоде</button>
-                <button onClick={()=>setCards(clickTwoButton(cards))}>Карта в этой коллоде</button>
-                <button onClick={()=>setCards(clickTreeButton(cards))}>Карта в этой коллоде</button>
+                {state.isOpenButtons && (<div className="startButtonsField">
+                    <button onClick={()=>setCards(clickOneButton(cards))}>Карта в этой коллоде</button>
+                    <button onClick={()=>setCards(clickTwoButton(cards))}>Карта в этой коллоде</button>
+                    <button onClick={()=>setCards(clickTreeButton(cards))}>Карта в этой коллоде</button>                    
+                </div>)}
+                {/* <Modal imageURL={cards[7].imageURL} altImage={cards[7].title}  /> */}
+                <div>
+                    {state.isOpenButton && <button onClick={()=>setState({isOpenModal: true})}>Ответ</button>}
+                    {state.isOpenModal && (<div className='modal'>
+                        <div className='modal-body'>
+                            <h1>Это ваша карта?</h1>
+                            <img className='ansverCard' src={cards[7].imageURL} alt={cards[7].title} />
+                            <button onClick={()=>setState({isOpenModal: false, isOpenButton: false, isOpenButtons: true})}>Играть заново</button>
+                        </div>            
+                    </div>)}
                 </div>
-                <Modal imageURL={cards[7].imageURL} altImage={cards[7].title}/>
             </div>  
         </div>       
     )
--- src/mainGame/modal.js
+++ src/mainGame/modal.js
@@ -3,16 +3,17 @@ import './modal.css';
 
 export default function Modal(props){
     const [state, setState] = useState (
-        {isOpen: false}
+        {isOpenModal: false, isOpenButton: true}
     )
+    
     return (
         <div>
-            <button onClick={()=>setState({isOpen: true})}>Ответ</button>
-            {state.isOpen && (<div className='modal'>
+            {state.isOpenButton && <button onClick={()=>setState({isOpenModal: true})}>Ответ</button>}
+            {state.isOpenModal && (<div className='modal'>
                 <div className='modal-body'>
                     <h1>Это ваша карта?</h1>
                     <img className='ansverCard' src={props.imageURL} alt={props.altImage} />
-                    <button onClick={()=>setState({isOpen: false})}>Играть заново</button>
+                    <button onClick={()=>setState({isOpenModal: false, isOpenButton: true})}>Играть заново</button>
                 </div>            
             </div>)}
         </div>
